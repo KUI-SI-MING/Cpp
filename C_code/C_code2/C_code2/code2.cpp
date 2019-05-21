e39d1a0eb136eb57deb145a6a4a16adde216f5dd
KUI-SI-MING <kuisi5495@163.com>
2019-05-21 21:37:42 +0800

    C_code2

--- C_code/C_code2/C_code2/code2.cpp
+++ C_code/C_code2/C_code2/code2.cpp
@@ -133,12 +133,12 @@ void Determine_OE()
 		}
 	}
 
-	cout << "The even of elements is: ";
+	cout << "The even bit of elements is: ";
 	for (int i = 0; i < even.size(); i++)
 	{
 		cout << ' ' << even[i];
 	} 
-	cout << endl << "The odd of elements is: ";
+	cout << endl << "The odd bit of elements is: ";
 	for (int i = 0; i < odd.size(); i++)
 	{
 		cout << ' ' << odd[i];
@@ -173,9 +173,65 @@ void DifferentBit()
 
 ////////////////////////////////////////////////////////
 //6.跳水比赛
+//5位运动员参加了10米台跳水比赛，有人让他们预测比赛结果
+//A选手说：B第二，我第三；
+//B选手说：我第二，E第四；
+//C选手说：我第一，D第二；
+//D选手说：C最后，我第三；
+//E选手说：我第四，A第一；
+//比赛结束后，每位选手都说对了一半，请编程确定比赛的名次
+int Forecast(int arr[])//判断预测结果
+{
+	if ((arr[1] == 2) + (arr[0] == 3) == 1
+		&& (arr[1] == 2) + (arr[4] == 4) == 1
+		&& (arr[2] == 1) + (arr[3] == 2) == 1
+		&& (arr[2] == 5) + (arr[3] == 3) == 1
+		&& (arr[4] == 4) + (arr[0] == 1) == 1)
+		return 1;
+	else
+		return 0;
+}
+int Repetition(int arr[])//判断名次是否重复
+{
+	int i = 0;
+	int j = 0;
+	for (i = 0; i < 5; i++)
+		for (j = i + 1; j < 5; j++)
+		{
+			if (arr[i] == arr[j])
+				return 0;
+		}
+	return 1;
+}
 void MatchRanking()
 {
-
+	int arr[5] = { 0 };
+	//数组arr[0]~arr[4]分别代表A B C D E  
+	//arr[i]的值表示其名次  
+	for (arr[0] = 1; arr[0] <= 5; arr[0]++)
+	{
+		for (arr[1] = 1; arr[1] <= 5; arr[1]++)
+		{
+			for (arr[2] = 1; arr[2] <= 5; arr[2]++)
+			{
+				for (arr[3] = 1; arr[3] <= 5; arr[3]++)
+				{
+					for (arr[4] = 1; arr[4] <= 5; arr[4]++)
+					{
+						if (Forecast(arr) == 1)
+							if (Repetition(arr) != 0)
+							{
+								cout << "A:为第 " << arr[0] << "名" << endl;
+								cout << "B:为第 " << arr[1] << "名" << endl;
+								cout << "C:为第 " << arr[2] << "名" << endl;
+								cout << "D:为第 " << arr[3] << "名" << endl;
+								cout << "E:为第 " << arr[4] << "名" << endl;
+							}
+					}
+				}
+			}
+		}
+	}
 }
 int main()
 {
