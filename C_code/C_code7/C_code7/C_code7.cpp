c93af2f57f155211edfd95df189731ee8233dde4
KUI-SI-MING <kuisi5495@163.com>
2019-07-26 19:08:13 +0800

    C_code7

--- C_code/C_code7/C_code7/C_code7.cpp
+++ C_code/C_code7/C_code7/C_code7.cpp
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 
 using namespace std;
+#define N 100
 
 ////////////////////////////////////////////////////////////
 //1.键盘输入任意整数 n，通过程序运行输出对应高度为 n 的等腰三角形
@@ -26,10 +27,59 @@ void Trangle()
 		cout << endl;
 	}
 }
+
+////////////////////////////////////////////////////
+//2.编号为 1，2，3，…，n 的 n 个人围坐一圈，任选一个正整数 m 作为报数上限值，
+//从第一个人开始按顺时针方向报数，报数到 m 时停止，报数为 m 的人出列。
+//从出列人的顺时针方向的下一个人开始又从 1 重新报数，如此下去，直到所有人都全部出列为止
+void _joseph(int a[], int n, int m)
+{
+	int k = 0;
+	int j = 0;
+
+	for (int i = 0; i < n; i++)
+	{
+		j = 1;
+		while (j < m)
+		{
+			while (a[k] == 0)
+				k = (k + 1) % n;
+			j++;
+			k = (k + 1) % n;
+		}
+
+		while (a[k] == 0)
+			k = (k + 1) % n;
+		cout << a[k] << ' ';
+		a[k] = 0;
+	}
+}
+
+void Joseph()
+{
+	int da[N] = { 0 };
+	int num = 0;
+	int count = 0;
+	cout << "Please input num & count: ";
+	cin >> num >> count;
+
+	for (int i = 0; i < num; i++)
+	{
+		da[i] = i + 1;
+	}
+
+	cout << endl << "The output is " << endl;
+	_joseph(da, num, count);
+	cout << endl;
+
+}
 int main()
 {
 	////1.输出等腰三角形
-	Trangle();
+	//Trangle();
+	////2.约瑟夫环
+	Joseph();
+
 	system("pause");
 	return 0;
 }
